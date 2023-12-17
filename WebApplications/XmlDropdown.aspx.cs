a86cd8fb6895683af0838f70647e1c8edf715934
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-17 18:14:10 +0300

    XmlDropdown

---
+++ WebApplications/XmlDropdown.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplications
+{
+    public partial class XmlDropdown : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataSet DS = new DataSet();
+
+            string strPhysicalPath = Server.MapPath("Countries.xml");
+
+            DS.ReadXml(strPhysicalPath);
+            DropDownList1.DataSource = DS;
+            DropDownList1.DataTextField = "CountryName";
+            DropDownList1.DataValueField = "CountryId";
+            DropDownList1.DataBind();
+
+            ListItem li = new ListItem("Select", "-1");
+            //initial value
+            DropDownList1.Items.Insert(0,li);
+
+        }
+    }
+}
