4f8f333643a54c55352b7f1e239ea2459f3345cb
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-17 19:08:02 +0300

    CascadingDropdown

---
+++ WebApplications/CascadingDropdown.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CascadingDropdown.aspx.cs" Inherits="WebApplications.CascadingDropdown" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <%--autoPostBack olmazsa çalışmaz, Çünkü cached eventi autoPostBack e çevirmemiz gerekir--%>
+            <asp:DropDownList ID="ddlCategories" Width="100px" runat="server" OnSelectedIndexChanged="ddlCategories_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+            <br />
+            <asp:DropDownList ID="ddlProducts" Width="100px" runat="server"></asp:DropDownList>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplications/CascadingDropdown.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplications
+{
+    public partial class CascadingDropdown : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack) 
+            {
+                ddlCategories.DataSource =  GetData("spGetCategories", null);
+                ddlCategories.DataValueField = "CategoryId";
+                ddlCategories.DataTextField = "CategoryName";
+                ddlCategories.DataBind();
+
+                ListItem liCategory = new ListItem("Select Category", "-1");
+                ddlCategories.Items.Insert(0, liCategory);
+                ListItem liProducts = new ListItem("Select Product", "-1");
+                ddlProducts.Items.Insert(0, liProducts);
+
+                ddlProducts.Enabled = false;    
+            }
+        }
+        private DataSet GetData(string SPName, SqlParameter SPParameter)
+        {
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            SqlConnection con = new SqlConnection(CS);
+            SqlDataAdapter da = new SqlDataAdapter(SPName, con);
+
+            if(SPParameter != null )
+            {
+                da.SelectCommand.Parameters.Add(SPParameter);
+            }
+
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            DataSet DS = new DataSet();
+
+            da.Fill(DS);
+            return DS;
+        }
+
+        protected void ddlCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(ddlCategories.SelectedIndex == 0)
+            {
+                ddlProducts.Enabled = false;
+                ddlProducts.SelectedValue = "0";
+            }
+            else
+            {
+                ddlProducts.Enabled = true;
+                SqlParameter parameter = new SqlParameter("@CategoryId", ddlCategories.SelectedValue);
+                DataSet DS = GetData("spGetProductsByCategoryId", parameter);
+
+                ddlProducts.DataSource = DS;
+                ddlProducts.DataTextField = "ProductName";
+                ddlProducts.DataValueField = "ProductId";
+                ddlProducts.DataBind();
+                ListItem liProducts = new ListItem("Select Product", "-1");
+                ddlProducts.Items.Insert(0, liProducts);
+
+            }
+            
+        }
+    }
+}
