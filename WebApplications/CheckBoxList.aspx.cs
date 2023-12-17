0d798ea255eaeab73c22d5b7f3f526e5494fb152
yeyilmaz99 <yeyilmaz34@gmail.com>
2023-12-17 19:26:16 +0300

    CheckBoxList

---
+++ WebApplications/CheckBoxList.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplications
+{
+    public partial class CheckBoxList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            foreach(ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+                {
+                    Response.Write(li.Text + "<br/>");
+                    Response.Write(li.Value + "<br/>");
+                }
+            }
+        }
+    }
+}
